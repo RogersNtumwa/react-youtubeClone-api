f47a58b939900c86f859cf30f3bafe416bda2069
Rogers Ntumwa <ntumwar@gmail.com>
2020-07-27 13:38:53 +0400

    implemented form handling in the search component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,29 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  state = { searchTerm: "" };
+
+  searchHandler=(event) =>{
+    this.setState({
+      searchTerm: event.target.value,
+    });
+  }
+
   render() {
-    return <div className="">Search Bar</div>;
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label htmlFor="search">Video Search</label>
+            <input
+              type="text"
+              value={this.state.searchTerm}
+              onChange={this.searchHandler}
+            />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
